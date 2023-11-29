b25992e35895bcbce63f816d327251790a315c37
Dionisis Konstantinos Kapsimalis <vpddkf@gmail.com>
2023-11-29 13:01:16 +0200

    Take heatmap data from firebase

--- calendarHeatMap.js
+++ calendarHeatMap.js
@@ -1,11 +1,7 @@
-async function drawHeatMap() {
-
-    var dataset;
-
+async function drawHeatMap(dataset) {
     try {
-        // Load the data asynchronously
-        dataset = await d3.json("https://raw.githubusercontent.com/Dionkaps/test/main/data.json");
-        console.log(JSON.stringify(dataset, null, 2)); // Log the loaded data as a JSON string
+        // Log the loaded data as a JSON string
+        console.log(JSON.stringify(dataset, null, 2));
 
         // Your code using the 'dataset' goes here
 
@@ -13,137 +9,127 @@ async function drawHeatMap() {
         var months = d3.groups(dataset, d => d.MONTHID);
         var months_name = d3.groups(dataset, d => d.M_NAME);
 
-        // Rest of your code...
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
+        var week = ["Sun", "Tue", "Thu", "Sat"];
+        var legends = ["Balance Pending", "No Order Recieved", "Order Recieved"];
 
-    // dataset grouping at different levels
+        var box = 12; //cell size - ideal is 12 to 25
 
-    var months = d3.groups(dataset, d => d.MONTHID);
-    var months_name = d3.groups(dataset, d => d.M_NAME);
-    var week = ["Sun", "Tue", "Thu", "Sat"];
-    var legends = ["Balance Pending", "No Order Recieved", "Order Recieved"];
-
-    var box = 12; //cell size - ideal is 12 to 25
-
-    //Dimensions
-    var dim = {
-        width: months.length * 80 + 360,
-        height: box * 7 + 80,
-        margin: {
-            top: 10,
-            bottom: 10,
-            left: 10,
-            right: 10
+        //Dimensions
+        var dim = {
+            width: months.length * 80 + 360,
+            height: box * 7 + 80,
+            margin: {
+                top: 10,
+                bottom: 10,
+                left: 10,
+                right: 10
+            }
         }
-    }
 
-    dim.ctrWidth = dim.width - dim.margin.left - dim.margin.right;
-    dim.ctrHeight = dim.height - dim.margin.top - dim.margin.bottom;
+        dim.ctrWidth = dim.width - dim.margin.left - dim.margin.right;
+        dim.ctrHeight = dim.height - dim.margin.top - dim.margin.bottom;
 
-    // Accessor Functions:
-    var balanceFlagAccessor = (d) => d.BAL_FLAG;
-    var dayNameAccessor = (d) => d.DAY_NAME_SHORT;
-    var orderAmountAccessor = (d) => d.TOTAL_AMOUNT;
-    var balanceAmountAccessor = (d) => (d.TOTAL_AMOUNT - d.DISCOUNT - d.RECIEVED_AMOUNT);
-    var dateAccessor = (d) => d.DB_DATE;
-    var weekDayNoAccessor = (d) => d.WEEKDAYNO;
-    var weekOfMonthAccessor = (d) => d.WEEK_OF_MONTH;
+        // Accessor Functions:
+        var balanceFlagAccessor = (d) => d.BAL_FLAG;
+        var dayNameAccessor = (d) => d.DAY_NAME_SHORT;
+        var orderAmountAccessor = (d) => d.TOTAL_AMOUNT;
+        var balanceAmountAccessor = (d) => (d.TOTAL_AMOUNT - d.DISCOUNT - d.RECIEVED_AMOUNT);
+        var dateAccessor = (d) => d.DB_DATE;
+        var weekDayNoAccessor = (d) => d.WEEKDAYNO;
+        var weekOfMonthAccessor = (d) => d.WEEK_OF_MONTH;
 
-    // colorScale
-    var colorScale = d3.scaleQuantize()
-        .domain([0, 3])
-        .range(['#dfe1e4', '#80e49a', '#40c463', '#19cf49']);
+        // colorScale
+        var colorScale = d3.scaleQuantize()
+            .domain([0, 3])
+            .range(['#dfe1e4', '#80e49a', '#40c463', '#19cf49']);
 
-    //Parent SVG
-    var svg = d3.select('#orderHeatMap')
-        .append('svg')
-        .attr('width', dim.width)
-        .attr('height', dim.height)
-        .classed('svg-area', true);
+        var screenWidth = window.innerWidth;
+        //Parent SVG
+        var svg = d3.select('#orderHeatMap')
+            .append('svg')
+            .attr('width', screenWidth)
+            .attr('height', dim.height)
+            .classed('svg-area', true);
 
-    //container
-    var ctr = svg.append('g')
-        .attr('transform', `translate(${dim.margin.left},${dim.margin.top})`);
+        //container
+        var ctr = svg.append('g')
+            .attr('transform', `translate(${dim.margin.left},${dim.margin.top})`);
 
+        //Heat Maps Container
+        var heatMapCtr = ctr.append("g")
+            .attr('transform', `translate(50,30)`);
 
-    //Heat Maps Container
-    var heatMapCtr = ctr.append("g")
-        .attr('transform', `translate(50,30)`);
+        // Week Labels
+        var weekGroup = ctr.append("g")
+            .attr("transform", `translate(0,${dim.margin.top + 50})`)
+            .selectAll("text")
+            .data(week)
+            .join("text")
+            .style("font-size", `12px`)
+            .text((d) => d)
+            .attr("y", (d, i) => (box * 2) * (i % 4))
+            .attr("x", 0);
 
-    // Week Labels
-    var weekGroup = ctr.append("g")
-        .attr("transform", `translate(0,${dim.margin.top + 50})`)
-        .selectAll("text")
-        .data(week)
-        .join("text")
-        .style("font-size", `12px`)
-        .text((d) => d)
-        .attr("y", (d, i) => (box * 2) * (i % 4))
-        .attr("x", 0);
+        // loop to render heatmaps
 
-    // loop to render heatmaps
+        for (j = 0; j < months.length; j++) {
 
-    for (j = 0; j < months.length; j++) {
+            // Month Labels
+            heatMapCtr.append("g")
+                .attr("transform", `translate(${j * (box * 6 + 20)},10)`)
+                .append("text")
+                .attr("x", 0)
+                .classed("heatMapMonths-labels", true)
+                .text(months_name[j][0]);
 
-        // Month Labels
-        heatMapCtr.append("g")
-            .attr("transform", `translate(${j * (box * 6 + 20)},10)`)
-            .append("text")
-            .attr("x", 0)
-            .classed("heatMapMonths-labels", true)
-            .text(months_name[j][0]);
+            // Monthly Heatmap
+            heatMapCtr.append("g")
+                .attr("transform", `translate(${j * (box * 6 + 20)},20)`)
+                .selectAll("rect")
+                .data(months[j][1])
+                .join("rect")
+                .attr("width", box)
+                .attr("height", box)
+                .attr("y", (d, i) => (box + 1) * ((weekDayNoAccessor(d)) % 7))
+                .attr("x", (d, i) => (box + 1) * (weekOfMonthAccessor(d)))
+                .attr("fill", (d) => colorScale(balanceFlagAccessor(d)))
+                .on("mouseenter", function (e) {
+                    d3.select(this)
+                        .attr("stroke", "black")
+                        .attr("stroke-width", 2);
+                })
+                .on("mouseleave", function (e) {
+                    d3.select(this)
+                        .attr("stroke", "black")
+                        .attr("stroke-width", 0);
+                })
+                .append("text")
+                .append("title")
+                .text((d) => "Date : " + dateAccessor(d) + ", Day : " + dayNameAccessor(d) + ", Sale : " + orderAmountAccessor(d) + ", Balance : " + balanceAmountAccessor(d))
+        }
 
-        // Monthly Heatmap
-        heatMapCtr.append("g")
-            .attr("transform", `translate(${j * (box * 6 + 20)},20)`)
-            .selectAll("rect")
-            .data(months[j][1])
+        // Legends
+        var legend = ctr.append("g")
+            .attr("transform", `translate(0,10)`);
+
+        legend.selectAll("rect")
+            .data(legends)
             .join("rect")
             .attr("width", box)
             .attr("height", box)
-            .attr("y", (d, i) => (box + 1) * ((weekDayNoAccessor(d)) % 7))
-            .attr("x", (d, i) => (box + 1) * (weekOfMonthAccessor(d)))
-            .attr("fill", (d) => colorScale(balanceFlagAccessor(d)))
-            .on("mouseenter", function (e) {
-                d3.select(this)
-                    .attr("stroke", "black")
-                    .attr("stroke-width", 2);
-            })
-            .on("mouseleave", function (e) {
-                d3.select(this)
-                    .attr("stroke", "black")
-                    .attr("stroke-width", 0);
-            })
-            .append("text")
-            .append("title")
-            .text((d) => "Date : " + dateAccessor(d) + ", Day : " + dayNameAccessor(d) + ", Sale : " + orderAmountAccessor(d) + ", Balance : " + balanceAmountAccessor(d))
+            .attr("x", (d, i) => 140 * (i % 3))
+            .attr("y", -11)
+            .attr("fill", (d, i) => colorScale(i - 1));
 
+        legend.selectAll("text")
+            .data(legends)
+            .join("text")
+            .attr("x", (d, i) => 140 * (i % 3) + box + 2)
+            .attr("y", 0)
+            .text((d) => d)
+            .style("font-size", "12px");
+
+    } catch (error) {
+        console.error("Error processing data:", error);
     }
-
-
-    // Legends
-    var legend = ctr.append("g")
-        .attr("transform", `translate(0,10)`);
-
-    legend.selectAll("rect")
-        .data(legends)
-        .join("rect")
-        .attr("width", box)
-        .attr("height", box)
-        .attr("x", (d, i) => 140 * (i % 3))
-        .attr("y", -11)
-        .attr("fill", (d, i) => colorScale(i - 1));
-
-    legend.selectAll("text")
-        .data(legends)
-        .join("text")
-        .attr("x", (d, i) => 140 * (i % 3) + box + 2)
-        .attr("y", 0)
-        .text((d) => d)
-        .style("font-size", "12px");
-
 }
-
-drawHeatMap();
--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, set, get, ref, child } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,10 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-import { getDatabase, set, get, update, remove, ref, child }
-    from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
-
 const db = getDatabase();
 
 // Accessing checkbox elements
@@ -28,6 +23,87 @@ var ateWellCheckbox = document.getElementById('ateWell');
 var trainedTodayCheckbox = document.getElementById('trainedToday');
 var submitButton = document.getElementById('submitBtn');
 
+var firebaseData = [];
+
+async function FindData() {
+    const dbref = ref(db);
+
+    try {
+        const snapshot = await get(child(dbref, "data/"));
+        if (snapshot.exists()) {
+            console.log(snapshot.val());
+            const dateListInput = snapshot.val();
+            firebaseData = Object.entries(dateListInput).map(([dateString, data]) => ({
+                date: new Date(dateString),
+                BAL_FLAG: data.BAL_FLAG
+            }));
+        } else {
+            alert("No data found");
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+function generateDataFromList(dateList) {
+    return dateList.map(({ date, BAL_FLAG }) => {
+        const DATEID = date.getFullYear() * 10000 + (date.getMonth() + 1) * 100 + date.getDate();
+        const DB_DATE = date.toISOString().split('T')[0];
+        const DAY = date.getDate();
+        const DAY_NAME_SHORT = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][date.getDay()];
+        const WEEKDAYNO = date.getDay();
+        const WEEKNO = getWeekNumber(date);
+        const WEEK_OF_MONTH = Math.floor((date.getDate() - 1) / 7);
+        const MONTHID = date.getFullYear() * 100 + (date.getMonth() + 1);
+        const M_NAME = date.toLocaleString('default', { month: 'short' }) + '-' + date.getFullYear().toString().slice(2);
+        const YEARID = date.getFullYear();
+        const ORDER_ID = "1"; // Replace with the actual value
+        const TOTAL_AMOUNT = "504"; // Replace with the actual value
+        const RECIEVED_AMOUNT = "500"; // Replace with the actual value
+        const DISCOUNT = "4"; // Replace with the actual value
+        const ORDER_DATE = `${('0' + date.getDate()).slice(-2)}-${('0' + (date.getMonth() + 1)).slice(-2)}-${date.getFullYear()}`;
+
+        return {
+            "DATEID": DATEID.toString(),
+            "DB_DATE": DB_DATE,
+            "DAY": DAY.toString(),
+            "DAY_NAME_SHORT": DAY_NAME_SHORT,
+            "WEEKDAYNO": WEEKDAYNO.toString(),
+            "WEEKNO": WEEKNO.toString(),
+            "WEEK_OF_MONTH": WEEK_OF_MONTH.toString(),
+            "MONTHID": MONTHID.toString(),
+            "M_NAME": M_NAME,
+            "YEARID": YEARID.toString(),
+            "ORDER_ID": ORDER_ID,
+            "TOTAL_AMOUNT": TOTAL_AMOUNT,
+            "RECIEVED_AMOUNT": RECIEVED_AMOUNT,
+            "DISCOUNT": DISCOUNT,
+            "ORDER_DATE": ORDER_DATE,
+            "BAL_FLAG": BAL_FLAG
+        };
+    });
+}
+
+function getWeekNumber(date) {
+    const startOfWeek = new Date(date.getFullYear(), 0, 1);
+    const days = Math.floor((date - startOfWeek) / (24 * 60 * 60 * 1000));
+    return Math.ceil((days + startOfWeek.getDay() + 1) / 7)
+}
+
+async function getData() {
+    try {
+        await FindData();
+        const generatedData = generateDataFromList(firebaseData);
+        console.log(generatedData);
+        drawHeatMap(generatedData);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getData();
+
+submitButton.addEventListener('click', submitData);
 
 function submitData() {
     // Storing checkbox values in variables
@@ -62,6 +138,3 @@ function submitData() {
             alert("Έγινε μαλακία επικοινωνήστε με τον CEIDA που το φτιαξε")
         });
 }
-
-submitButton.addEventListener('click', submitData);
-
